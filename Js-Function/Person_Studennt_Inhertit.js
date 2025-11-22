f7c34cb5ca8d51654bf564f000ab401bbb28f8de
Shashank shekhar <shashankshekhargiri2003@gmail.com>
2025-11-22 20:42:09 +0530

    [Shashank] added Person_Inheritance js file successfully

---
+++ Js-Function/Person_Studennt_Inhertit.js
@@ -0,0 +1,27 @@
+
+
+function Person(name) {
+    this.name = name;
+}
+
+Person.prototype.showName = function () {
+    console.log("Name:", this.name);
+};
+
+function Student(name, branch) {
+    Person.call(this, name);  
+    this.branch = branch;
+}
+
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.constructor = Student;
+
+Student.prototype.showBranch = function () {
+    console.log("Branch:", this.branch);
+};
+
+let student1 = new Student("Shashank", "Computer Science");
+
+student1.showName();     
+student1.showBranch();   
