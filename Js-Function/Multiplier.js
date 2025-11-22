0a9a6399b2dfb75100a488ac216e34b509830cfb
Shashank shekhar <shashankshekhargiri2003@gmail.com>
2025-11-22 20:45:47 +0530

    [Shashank] added Makemultiplier js file successfully

---
+++ Js-Function/Multiplier.js
@@ -0,0 +1,10 @@
+function makeMultiplier(multiplier) {
+
+    return function (num) {
+        return num * multiplier;   
+    };
+
+}
+
+const triple = makeMultiplier(3);
+console.log(triple(5));  // 15
